d1dc0d94ee7b417a45b70ba6e0c5b0d757ad7813
Harshdeep Singh Gurtatta <harsh10deep01@gmail.com>
2023-05-30 03:42:07 +0530

    check the element in the array

---
+++ 8kyu/array-conatins-element.js
@@ -0,0 +1,13 @@
+// check whether the array contains value without looping over it!
+
+
+// my solution
+
+function check(a,x){
+    if(a.includes(x)) {
+      return true
+    }else {
+      return false
+    }
+  };
+
