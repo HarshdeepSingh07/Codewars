f093cb07867ed87ef16bb33104ddda7484f0c7c5
Harshdeep Singh Gurtatta <harsh10deep01@gmail.com>
2023-07-03 23:39:15 +0530

    new codewars added

---
+++ 8kyu/filling-gaps.js
@@ -0,0 +1,13 @@
+// filling the gaps in the given array
+
+
+function pipeFix(numbers){
+    var first = numbers[0];
+    var last = numbers[numbers.length-1];
+    
+    var arr = [];
+    for(var i = first; i <= last; i++) {
+      arr.push(i);
+    }
+    return arr;
+  }
