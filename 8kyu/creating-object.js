c83d270b7e185a8400a12cbcd6baf7d6d030b23d
Harshdeep Singh Gurtatta <harsh10deep01@gmail.com>
2023-07-10 03:06:05 +0530

    creating getter in class and implementing it!

---
+++ 8kyu/creating-object.js
@@ -0,0 +1,15 @@
+// crate an object an get info through getter
+
+
+class Person {
+
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+    
+    get info() {
+      return `${this.name}s age is ${this.age}`;
+    }
+    
+  }
