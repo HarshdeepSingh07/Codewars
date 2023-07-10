4b145ed0ff0f73b6c933e89c1e06c23de67d30d8
Harshdeep Singh Gurtatta <harsh10deep01@gmail.com>
2023-07-11 02:26:08 +0530

    sorting out every kind of case from string

---
+++ 8kyu/filtering-cases.js
@@ -0,0 +1,11 @@
+// filtering uppercase , lowercase, integers, and special letters in given string
+
+
+
+const solve = x => {
+    let u = x.match(/[A-Z]/g)||[]
+    let d = x.match(/[a-z]/g)||[]
+    let n = x.match(/[0-9]/g)||[]
+    let s = x.match(/[^A-Z0-9]/gi)||[]
+    return [u.length, d.length, n.length, s.length]
+  }
